44d772377be05ce22297cc47003a42d3a9ad7692
dkonoshe <sbn32meat>
2019-01-09 09:20:35 +0300

    finish AddressDao test's

--- src/test/java/test/project/dao/implimintation/AddressDaoImplTest.java
+++ src/test/java/test/project/dao/implimintation/AddressDaoImplTest.java
@@ -1,9 +1,9 @@
 package test.project.dao.implimintation;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -15,26 +15,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({App.class, AddressDaoImpl.class})
 public class AddressDaoImplTest {
+	@InjectMocks
+	private AddressDaoImpl addressDao;
 
-	private static final String DB_URL = "jdbc:h2:tcp://localhost/~/test";
-	private static final String DB_USERNAME = "sa";
-	private static final String DB_PASSWORD = "";
-	private String sql = "SELECT ID, COUNTRY, CITY, STREET, POST_CODE FROM ADDRESS WHERE ID=?";
-	@InjectMocks private AddressDaoImpl addressDao;
+	private Connection connection;
+	private PreparedStatement preparedStatement;
+	private ResultSet resultSet;
+	private Long id = 1L;
+	private Address address;
+
+	@Before
+	public void init() throws SQLException {
+		connection = PowerMockito.mock(Connection.class);
+		preparedStatement = PowerMockito.mock(PreparedStatement.class);
+		resultSet = PowerMockito.mock(ResultSet.class);
+		PowerMockito.mockStatic(App.class);
+		PowerMockito.when(App.getConnection()).thenReturn(connection);
+		PowerMockito.when(connection.prepareStatement(any(String.class))).thenReturn(preparedStatement);
+		PowerMockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+		address = new Address();
+		address.setCity("Voronej");
+		address.setCountry("RUS");
+		address.setPostCode("123123");
+		address.setStreet("Milk");
+		address.setId(1L);
+	}
 
 	@Test
 	public void getById() throws SQLException {
-		MockitoAnnotations.initMocks(this);
-		Connection connection = PowerMockito.mock(Connection.class);
-		PreparedStatement preparedStatement = PowerMockito.mock(PreparedStatement.class);
-		ResultSet resultSet = PowerMockito.mock(ResultSet.class);
-		PowerMockito.mockStatic(App.class);
-		PowerMockito.when(App.getConnection()).thenReturn(connection);
-		PowerMockito.when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
-		PowerMockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
 		PowerMockito.when(resultSet.next()).thenReturn(true);
 		PowerMockito.when(resultSet.getLong("ID")).thenReturn(1L);
 		PowerMockito.when(resultSet.getString("COUNTRY")).thenReturn("Bel");
@@ -42,7 +57,17 @@ public class AddressDaoImplTest {
 		PowerMockito.when(resultSet.getString("STREET")).thenReturn("Fer");
 		PowerMockito.when(resultSet.getString("POST_CODE")).thenReturn("123");
 		Address address = addressDao.getById(1L);
-		System.out.println(address);
+		assertEquals(address.getId(), id);
+	}
+
+	@Test
+	public void update() {
+		addressDao.update(address);
+	}
+
+	@Test
+	public void remove() {
+		addressDao.remove(address);
 	}
 
 }
