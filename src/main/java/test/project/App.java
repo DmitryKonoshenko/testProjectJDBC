89d193f2b67450f0779475a2160e2c44296403bc
dkonoshe <sbn32meat>
2019-01-09 10:18:53 +0300

    delete unnecessary code

--- src/main/java/test/project/App.java
+++ src/main/java/test/project/App.java
@@ -31,48 +31,4 @@ public class App {
 		}
 		return connection;
 	}
-
-	public static void main(String[] args) throws SQLException {
-		PersonDao personDao = new PersonDaoImpl();
-//		Address address = new Address();
-//		address.setCity("Voronej");
-//		address.setCountry("RUS");
-//		address.setPostCode("123123");
-//		address.setStreet("Milk");
-//
-//		Person person = new Person();
-//		person.setFirstName("Denchik");
-//		person.setLastName("Por");
-//		person.setBirthday(LocalDate.of(1995,3,9));
-//		person.setAddress(address);
-
-//		Person person2 = new Person();
-//		person2.setFirstName("Denchik");
-//		person2.setLastName("Por2");
-//		person2.setBirthday(LocalDate.of(1995,3,9));
-//		person2.setAddress(address);
-//
-//		personDao.add(person);
-//		personDao.add(person2);
-//		List<Person> p = DaoLocatorFactory.getDaoLocator().getPersonDao().getAll();
-//		p.forEach(System.out::println);
-//		System.out.println(personDao.getByName("Denchik"));
-
-//		Address address = new Address();
-//		address.setId(1L);
-//		address.setCity("Piter");
-//		address.setCountry("Her");
-//		address.setPostCode("123123");
-//		address.setStreet("Milk");
-//
-//		Person person = new Person();
-//		person.setId(1L);
-//		person.setFirstName("Mark");
-//		person.setLastName("Cot");
-//		person.setBirthday(LocalDate.of(1996, 3, 9));
-//		person.setAddress(address);
-//		personDao.update(person);
-		Person person = DaoLocatorFactory.getDaoLocator().getPersonDao().getByName("Denchik");
-		personDao.remove(person);
-	}
 }
